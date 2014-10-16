d20fa0f5e90f421b91c8eba8177cd9dea18b2c1c
Brian Leppez <brianleppez@wireless-10-147-223-36.public.utexas.edu>
2014-10-16 16:03:46 -0500

    added call feature

---
+++ Alpha/Alpha/ATaxiTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATaxiTableViewController.h
+//  Alpha
+//
+//  Created by Brian Leppez on 10/16/14.
+//  Copyright (c) 2014 cs378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATaxiTableViewController : UITableViewController
+
+@end
