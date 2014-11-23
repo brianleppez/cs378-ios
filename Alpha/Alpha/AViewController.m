33b411cf0fb90abd35f004c5c5844e0d68bea88a
samanthaallen <allen.c.samantha@gmail.com>
2014-11-23 15:37:45 -0600

    More of Brian's map fixes

--- Alpha/Alpha/AViewController.m
+++ Alpha/Alpha/AViewController.m
@@ -108,15 +108,6 @@
     pins = nil;
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    CLLocationCoordinate2D coord = {.latitude =  30.2669444, .longitude =  -97.7427778};
-    
-    MKCoordinateRegion viewRegion  = MKCoordinateRegionMakeWithDistance(coord, 7.5*METERS_PER_MILE,7.5*METERS_PER_MILE);
-    
-    [mapView setRegion:viewRegion animated:YES];
-
-}
-
 -(NSMutableArray *) createAnnotations:(NSDictionary*)json {
     NSMutableArray *annotations = [[NSMutableArray alloc] init];
     
@@ -143,19 +134,6 @@
     
 }
 
-
-
-- (void)zoomToLocation
-{
-    CLLocationCoordinate2D zoomLocation;
-    zoomLocation.latitude = 30.2669444;
-    zoomLocation.longitude= -97.7427778;
-    
-    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 7.5*METERS_PER_MILE,7.5*METERS_PER_MILE);
-    [mapView setRegion:viewRegion animated:YES];
-    
-    [mapView regionThatFits:viewRegion];
-}
 
 - (NSNumber *)getBatteryLevel
 {
